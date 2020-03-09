84894278f49f1b293ca17e3fb2ccd4286d317a4b
Duyx <sandersen98@live.dk>
2020-03-09 15:59:01 +0100

    login

---
+++ ReScribe/Login/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  ReScribe
+//
+//  Created by Simon Andersen on 09/03/2020.
+//  Copyright © 2020 Simon Andersen. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
