fa3048d4302bbc66f818cf3ab34b838108b2775c
ChrisMizz <Christofferdetlef@gmail.com>
2020-03-30 07:32:38 +0200

    Implementering af søge funktion til oprettelse af nyt abonnoment

--- ReScribe/Main/SubscriptionAddViewController.swift
+++ ReScribe/Main/SubscriptionAddViewController.swift
@@ -21,11 +21,17 @@ extension UIColor {
 }
 
 class SubscriptionAddViewController: UIViewController {
-    @IBOutlet weak var infotabView: UIView!
+    
+    @IBOutlet weak var tblView: UITableView!
+    @IBOutlet weak var searchBarUI: UISearchBar!
+    
+    let subsNameArr = ["Viaplay", "Netflix", "HBO", "Youtube", "CBS", "Twitch", "Cmore", "D-play", "Spotify", "Apple Music", "World of Warcraft", "Apple TV", "Discord", "Strava", "Disney+", "Amazon Prime"]
+    
+    var searchName = [String]()
+    var searching = false
     override func viewDidLoad() {
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
         super.viewDidLoad()
-        self.infotabView.round(corners: [.bottomLeft, .bottomRight], cornerRadius: 20)
+        self.searchBarUI.round(corners: [.bottomLeft, .bottomRight, .topLeft, .topRight], cornerRadius: 10)
         self.navigationController?.navigationBar.barTintColor = UIColor.init(netHex: 0x353535)
     }
     override func viewWillDisappear(_ animated: Bool) {
@@ -34,5 +40,39 @@ class SubscriptionAddViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+}
+
+extension SubscriptionAddViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return searchName.count
+        } else {
+            return subsNameArr.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if searching {
+            cell?.textLabel?.text = searchName[indexPath.row]
+        } else {
+            cell?.textLabel?.text = subsNameArr[indexPath.row]
+        }
+        return cell!
+    }
+}
+
+extension SubscriptionAddViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchName = subsNameArr.filter({$0.prefix(searchText.count) == searchText})
+        searching = true
+        tblView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        tblView.reloadData()
     }
 }
