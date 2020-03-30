3e4b46a41d1dd52931b82fde57255184a7a40e92
Duyx <sandersen98@live.dk>
2020-03-30 08:46:30 +0200

    Login system

--- Podfile
+++ Podfile
@@ -8,6 +8,9 @@ target 'ReScribe' do
   # Pods for ReScribe
   # add the Firebase pod for Google Analytics 
   pod 'Firebase/Analytics'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Core'
+  pod 'Firebase/Firestore'
   # add pods for any other desired Firebase products
   # https://firebase.google.com/docs/ios/setup#available-pods
 
--- ReScribe/Login/LoginViewController.swift
+++ ReScribe/Login/LoginViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import Foundation
+import UIKit
+import FirebaseAuth
 import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var insertEmail: UITextField!
+    @IBOutlet weak var insertPass: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var databaseLoad: UIActivityIndicatorView!
+    @IBOutlet weak var viewHolder: UIView!
+    var errorOcurred = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.viewHolder.round(corners: [.bottomLeft, .bottomRight, .topLeft, .topRight], cornerRadius: 20)
     }
 
+    @IBAction func loginTapped(_ sender: Any) {
 
+        if (insertEmail.text?.isEmpty)! || (insertPass.text?.isEmpty)! {
+            errorLabel.alpha = 1
+            errorLabel.text = "email/password cant be empty"
+        } else {
+            errorLabel.text = ""
+            databaseLoad.alpha = 1
+            databaseLoad.startAnimating()
+            
+            let email = self.insertEmail.text!
+            let password = self.insertPass.text!
+            
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                if error != nil {
+                    self.errorLabel.alpha = 1
+                    self.errorLabel.text = error!.localizedDescription
+                    self.databaseLoad.stopAnimating()
+                    self.databaseLoad.alpha = 0
+                } else {
+                    
+                    self.databaseLoad.stopAnimating()
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let vc = storyboard.instantiateViewController(identifier: "mainVC")
+                   
+                    self.view.window?.rootViewController = vc
+                    self.view.window?.makeKeyAndVisible()
+                    
+                    /*let homeViewController = self.storyboard?.instantiateViewController(identifier: "HomeVC") as? HomeViewController
+                    self.view.window?.rootViewController = homeViewController
+                    self.view.window?.makeKeyAndVisible()*/
+                }
+            }
+            
+        }
+    }
 }
