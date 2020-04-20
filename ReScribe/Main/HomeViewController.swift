1359313597ad1ff0fedb39936f5fe43e5645c41d
ChrisMizz <Christofferdetlef@gmail.com>
2020-04-20 08:40:19 +0200

    The design for the home page is being made, and some functionality

--- ReScribe/Main/HomeViewController.swift
+++ ReScribe/Main/HomeViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
+let nameArr = ["Viaplay", "Netflix", "HBO", "Youtube", "CBS", "Twitch", "Cmore", "D-play", "Spotify", "Apple Music", "World of Warcraft", "Apple TV", "Discord", "Strava", "Disney+", "Amazon Prime"]
+
 class HomeViewController: UIViewController {
    
-    @IBOutlet weak var infotabView: UIView!
+    @IBOutlet weak var groupView: UIView!
+    @IBOutlet weak var inviView: UIView!
+    @IBOutlet weak var inviCell: UITableView!
+    @IBOutlet weak var individualTableView: UITableView!
+    @IBOutlet weak var infotabView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.inviView.round(corners: [.allCorners], cornerRadius: 10)
+        self.groupView.round(corners: [.allCorners], cornerRadius: 10)
         self.infotabView.round(corners: [.bottomLeft, .bottomRight], cornerRadius: 20)
+
+
     }
 }
 
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("hej")
+        return nameArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let inviCell = tableView.dequeueReusableCell(withIdentifier: "inviCell")
+        inviCell?.textLabel?.text = nameArr[indexPath.row]
+        
+        return inviCell!
+    }
+}
