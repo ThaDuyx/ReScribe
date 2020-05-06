3a9df778a2c27f817f1a2af6f3852c2e2d24a144
ChrisMizz <Christofferdetlef@gmail.com>
2020-05-06 15:51:38 +0200

    Register view made, and a few statements that does so you can't proceed with no email or password and wrong password

---
+++ ReScribe/Login/RegisterViewController.swift
@@ -0,0 +1,56 @@
+//
+//  RegisterViewController.swift
+//  ReScribe
+//
+//  Created by Christoffer Detlef on 06/05/2020.
+//  Copyright © 2020 Simon Andersen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var registerBtn: UIButton!
+    @IBOutlet weak var confirmPassLabel: UITextField!
+    @IBOutlet weak var passLabel: UITextField!
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.registerBtn.round(corners: .allCorners, cornerRadius: 10)
+        emailLabel.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        passLabel.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        confirmPassLabel.attributedPlaceholder = NSAttributedString(string: "Confirm Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+
+    }
+    @IBAction func goBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func enterTapped(_ sender: Any){
+        let password = passLabel.text!
+        let confirmPassword = confirmPassLabel.text!
+        let email = emailLabel.text!
+        if password != confirmPassword {
+            let refreshAlert = UIAlertController(title: "Error", message: "Password and Password confirm is not the same", preferredStyle: UIAlertController.Style.alert)
+            refreshAlert.addAction(UIAlertAction(title: "Try again", style: .cancel, handler: { (action: UIAlertAction!) in
+                
+            }))
+            self.present(refreshAlert, animated: true, completion: nil)
+        }else if password == "" {
+            let refreshAlert = UIAlertController(title: "Error", message: "Password can't be empty!", preferredStyle: UIAlertController.Style.alert)
+            refreshAlert.addAction(UIAlertAction(title: "Try again", style: .cancel, handler: { (action: UIAlertAction!) in
+                
+            }))
+            self.present(refreshAlert, animated: true, completion: nil)
+        } else if email == "" {
+            let refreshAlert = UIAlertController(title: "Error", message: "Email can't be empty!", preferredStyle: UIAlertController.Style.alert)
+            refreshAlert.addAction(UIAlertAction(title: "Try again", style: .cancel, handler: { (action: UIAlertAction!) in
+                
+            }))
+            self.present(refreshAlert, animated: true, completion: nil)
+        }else {
+            //Her skal vi skrive det kode som skal køres når man har skrevet email, password og alt er i orden
+        }
+    }
+}
